512cf29cfeea157efe62b61a67a56ae386130c2b
Ziyue Xia <200470612@student.georgianc.on.ca>
2021-10-13 01:29:11 -0400

    Created Location class model

---
+++ Models/Location.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chravel.Models
+{
+    public class Location
+    {
+        public int LocationId { get; set; }
+
+        [Required]
+        public string ProvinceName { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string District { get; set; }
+
+        [Required]
+        public string StreetName { get; set; }
+
+        [Required]
+        public string LocationName { get; set; }
+
+        [Required]
+        public string Detail { get; set; }
+
+        [Required]
+        public decimal AveragePricePerPerson { get; set; }
+
+        [Required]
+        public int TypeId { get; set; }
+    }
+}
