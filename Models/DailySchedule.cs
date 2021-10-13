3f990474b042c9d100d54aa45bc1894d69f8e8aa
Ziyue Xia <200470612@student.georgianc.on.ca>
2021-10-13 01:42:29 -0400

    Add Daily Schedule and Trip Schedule class models

--- Models/DailySchedule.cs
+++ Models/DailySchedule.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,21 @@ namespace Chravel.Models
 {
     public class DailySchedule
     {
+        public int ScheduleId { get; set; }
+
+        [Required]
+        public string Date { get; set; }
+
+        [Required]
+        public string Time { get; set; }
+
+        [Required]
+        public decimal Duration { get; set; }
+
+        [Required]
+        public int ActivityId { get; set; }
+
+        [Required]
+        public int TripId { get; set; }
     }
 }
---
+++ Models/TripSchedule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chravel.Models
+{
+    public class TripSchedule
+    {
+        public int TripId { get; set; }
+
+        public string TripName { get; set; }
+
+        public decimal Duration { get; set; }
+
+        public string UserId { get; set; }
+
+        //Add child ref to Daily schedule
+        
+    }
+}
